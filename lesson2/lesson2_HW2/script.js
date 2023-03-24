440eb2866ca6c0fd74f7af33d51f6e4aa2235154
KudrenkoMaksim <kudrenko.maksim3@gmail.com>
2023-03-24 12:05:59 +0200

    add home works lesson 2

---
+++ lesson2/lesson2_HW2/script.js
@@ -0,0 +1,3 @@
+let numberFirst=String(prompt('Введите 5-ти значное число'));
+const numberOver=String(numberFirst.charAt(0)+" "+numberFirst.charAt(1)+" "+numberFirst.charAt(2)+" "+numberFirst.charAt(3)+" "+numberFirst.charAt(4));
+console.log(numberOver);
