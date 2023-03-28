39579b66f118328064d1e6b474f17aff16cec313
KudrenkoMaksim <kudrenko.maksim3@gmail.com>
2023-03-28 11:11:05 +0300

    add lesson3_HW1

--- lesson2/lesson2_HW2/script.js
+++ lesson2/lesson2_HW2/script.js
@@ -1,3 +1,7 @@
-let numberFirst=String(prompt('Введите 5-ти значное число'));
+/*let numberFirst=String(prompt('Введите 5-ти значное число'));
 const numberOver=String(numberFirst.charAt(0)+" "+numberFirst.charAt(1)+" "+numberFirst.charAt(2)+" "+numberFirst.charAt(3)+" "+numberFirst.charAt(4));
-console.log(numberOver);
+console.log(numberOver);*/
+
+
+let a=12345;
+console.log (a.toString().split('').join(' '));
