87c8b9c12506f8672f83004f92e3ac87ca605127
KudrenkoMaksim <kudrenko.maksim3@gmail.com>
2023-03-28 15:06:23 +0300

    add HW 6 and 7

--- lesson3/lesson3_HW4/script.js
+++ lesson3/lesson3_HW4/script.js
@@ -1,8 +1,8 @@
 let a=Number(prompt('Введіть перше число',2));
 let b=Number(prompt('Введіть друге число',2));
 alert(
-`• ${String(a)}+${String(b)}=${Number(a+b)}
-• ${String(a)}-${String(b)}=${Number(a-b)}
-• ${String(a)}*${String(b)}=${Number(a*b)}
-• ${String(a)}/${String(b)}=${Number(a/b)}`
+`• ${(a)}+${(b)}=${(a+b)}
+• ${(a)}-${(b)}=${(a-b)}
+• ${(a)}*${(b)}=${(a*b)}
+• ${(a)}/${(b)}=${(a/b)}`
 )
--- lesson3/lesson3_HW5/script.js
+++ lesson3/lesson3_HW5/script.js
@@ -3,16 +3,16 @@ let a=Number(prompt('Enter first number'));
 let b=Number(prompt('Enter second number'));
 switch(operation){
    case 'add':
-      alert(`${String(a)}+${String(b)}=${Number(a+b)}`);
+      alert(`${(a)}+${(b)}=${(a+b)}`);
       break;
    case 'sub':
-      alert(`${String(a)}-${String(b)}=${Number(a-b)}`);
+      alert(`${(a)}-${(b)}=${(a-b)}`);
       break;
    case 'mult':
-      alert (`${String(a)}*${String(b)}=${Number(a*b)}`);
+      alert (`${(a)}*${(b)}=${(a*b)}`);
       break;
    case 'div':
-      alert(`${String(a)}/${String(b)}=${Number(a/b)}`)
+      alert(`${(a)}/${(b)}=${(a/b)}`)
       break;
    default:
       alert('unknown operation') 
