655697012845902a93de5265245300cbfa08c327
KudrenkoMaksim <kudrenko.maksim3@gmail.com>
2023-04-10 17:23:22 +0300

    hw14

--- lesson5/hw13/script.js
+++ lesson5/hw13/script.js
@@ -83,3 +83,11 @@
 //      return 0;
 // })
 // console.log(c) //масив з обнуленими значеннями, окрім 19го, який = 76
+   const arr=[16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+   function replaceWithMax(arr) {
+   const max = Math.max(...arr);
+      return arr.map((num) => (num === max ? num : 0));
+  
+    }
+    console.log(arr)
+  
