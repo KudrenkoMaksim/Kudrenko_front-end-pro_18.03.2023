ec722d541ad58395f4c2cd818205cfbe700ac176
KudrenkoMaksim <kudrenko.maksim3@gmail.com>
2023-04-13 15:01:59 +0300

    HomeWork-15

--- lesson3/lesson3_HW6/script.js
+++
@@ -1,3 +0,0 @@
-let a=Number(prompt('Введіть кількість годин'));
-const b=a*3600;
-alert(b.toString().concat(' секунд'));
--- lesson3/lesson3_HW7/script.js
+++
@@ -1,5 +0,0 @@
-let a=Number(prompt('Перше число'));
-let b=Number(prompt('Друге число'));
-let c=Number(prompt('Третє число'));
-alert(`Середнє арифметичне: ${(a+b+c)/3}`);
-
